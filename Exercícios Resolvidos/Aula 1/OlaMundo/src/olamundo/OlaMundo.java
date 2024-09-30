c3ef36c49231dc7566032027be5bccdacdf7594e
Daniel Pereira <danielp6r@gmail.com>
2024-09-30 16:42:07 -0300

    Update aula 01

---
+++ Exercícios Resolvidos/Aula 1/OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,16 @@
+package olamundo;
+
+/**
+ *
+ * @author daniel
+ */
+public class OlaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
